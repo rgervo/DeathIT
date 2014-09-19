f2357998fa15c4bf91fc5a9e566ef541e207bede
rgnix <rgervo@gmail.com>
2014-09-19 04:28:52 -0400

    added auto start on gate open

--- DeathIT.lua
+++ DeathIT.lua
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------------
--- Client Lua Script for DeathIT
--- Copyright (c) NCsoft. All rights reserved
+-- Wildstar Client Lua Script for DeathIT
+-- Swags - Pergo - Exile - ralph@rgnix.com
 -----------------------------------------------------------------------------------------------
  
 require "Window"
@@ -8,6 +8,7 @@ require "Tooltip"
 require "XmlDoc"
 require "GameLib"
 require "MatchingGame"
+require "ChatSystemLib"
  
 -----------------------------------------------------------------------------------------------
 -- DeathIT Module Definition
@@ -22,12 +23,14 @@ local defaultSettings = {
 		[1] = {0, -0, 0, -0},
 	  },
 	-- other settings
-	debug = false,
+	debug = true,
+	
 }
+
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
--- e.g. local kiExampleVariableMax = 999
+
  
 -----------------------------------------------------------------------------------------------
 -- Initialization
@@ -54,7 +57,7 @@ end
 
 -----------------------------------------------------------------------------------------------
 -- DeathIT OnLoad
------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------+++++++++++++++++++++++++++--------
 function DeathIT:OnLoad()
     -- load our form file
 	self.xmlDoc = XmlDoc.CreateFromFile("DeathIT.xml")
@@ -76,22 +79,23 @@ function DeathIT:OnDocLoaded()
 	    self.wndMain:Show(true, true)
 
 		-- if the xmlDoc is no longer needed, you should set it to nil
-		-- self.xmlDoc = nil
+		self.xmlDoc = nil
 		
 		-- Register handlers for events, slash commands and timer, etc.
-		-- e.g. Apollo.RegisterEventHandler("KeyDown", "OnKeyDown", self)
 		Apollo.RegisterSlashCommand("deathit", "OnDeathITOn", self)
-		
-		
+		Apollo.RegisterEventHandler("ChatMessage","OnChatMessage", self)
 		Apollo.RegisterTimerHandler("DeathTimer", "OnDeathTimer", self)
 		
 		-- Do additional Addon initialization here
 		self.nTimerProgress = nil
 		self.bDead = false
-		self.fTimeBeforeRezable = 30000		
+		self.fTimeBeforeRezable = 30000
+		self.maskSpawn = 0		
+		
 		Apollo.CreateTimer("DeathTimer", 0.10, true)
 		Apollo.StopTimer("DeathTimer")
 		
+		self:Debug("DeahtIT: ", "true")
 			
 	end
 end
@@ -111,6 +115,90 @@ end
  self.wndMain:FindChild("Title"):SetText(strTimeBeforeRezableFormatted .. Apollo.GetString("CRB__seconds"))
 
 end
+
+
+function DeathIT:OnChatMessage(channelCurrent, tMessage)
+	local message = tMessage.arMessageSegments[1].strText
+	
+	if channelCurrent:GetName() == "Instance" then	
+		self:Debug("Instance: ", "true")
+	end
+
+	if 	channelCurrent:GetName() == "System" then	
+		self:Debug("System: ", "true")
+	end
+	
+	if message == "You have been kicked by the server." then
+		self:Debug("MatchEnded: ", "true")
+		
+		-- Stop timer and reset things
+		Apollo.StopTimer("DeathTimer")
+		self.fTimeBeforeRezable = 30000
+		self.wndMain:FindChild("Title"):SetText("0.0 seconds")
+		self.maskSpawn = 0
+				
+	end
+
+	if channelCurrent:GetName() == "Datachron" then		
+		self:Debug("Datachron: ", "true")
+		
+		-- Moodie mask spawn. Fist spawn
+		if message == "A Moodie Mask has been unearthed!" and self.maskSpawn == 0 then
+			self.maskSpawn = self.maskSpawn + 1
+			self:Debug("Start timer", "mask timer")
+			-- start timer
+			Apollo.StartTimer("DeathTimer")
+		else 
+			self.maskSpawn = self.maskSpawn + 1
+		end
+		
+		self.Debug("TimerCount: ", self.maskSpawn) 
+	end
+
+end
+--[[
+function BGShouter:OnChatMessage(channelCurrent, tMessage)
+	local message = tMessage.arMessageSegments[1].strText
+	
+	if channelCurrent:GetName() == "Instance" then	
+		self:checkVersion(message)
+	end
+
+	if 
+		channelCurrent:GetName() == "System" or -- english/german
+		channelCurrent:GetName() == "Système" -- french
+	then	
+		self:gameCheck(message)	
+	end
+
+	if 
+		channelCurrent:GetName() == "System" or -- english/german
+		channelCurrent:GetName() == "Système" -- french
+	then	
+		if
+			message == "You have been kicked by the server." or -- english
+			string.find(message, "Vous avez été expulsé par le serveur") ~= nil or -- french
+			message == "Deine Verbindung mit dem Server wurde getrennt." -- german
+		then
+			if self.hidden then
+				self:OnClose()
+			end
+			self:ClearTracker()
+			self:prestigeCheck()
+		end	
+	end
+
+	if 
+		channelCurrent:GetName() == "Datachron" or -- english
+		channelCurrent:GetName() == "Infochron" -- german/french
+	then		
+		sendDebugMessage("datachron")
+		self:scoreCount(message)				
+		self:moodieMaskCheck(message)		
+		self:bloodsworn(message)
+	end
+end
+]]--
 
 -- on SlashCommand "/deathit"
 function DeathIT:OnDeathITOn()
@@ -152,7 +240,7 @@ end
 
 
 -----------------------------------------------------------------------------------------------
--- DeathITForm Functions
+-- DeathIT Form Functions
 -----------------------------------------------------------------------------------------------
 function DeathIT:OnStartTimer()
 	Apollo.StartTimer("DeathTimer")
@@ -174,6 +262,12 @@ end
 -- Debug
 -----------------------------------------------------------------------------------------------
 function DeathIT:Debug(message, error)
+	if message == nil then 
+		message = "nil"
+	elseif error == nil then 
+		error = "nil"
+	end
+		
 	if defaultSettings.debug == true then
 		Print(message .. error)
 	end
